d3fbe918c21a753143d8849caa47ef4fab02bce7
pawanpatil94 <pawanpatil94@gmail.com>
2017-06-25 20:46:39 -0700

    Initial commit

---
+++ app/src/main/java/com/example/finalproject/MainMenu.java
@@ -0,0 +1,17 @@
+package com.example.finalproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by pawan on 6/25/17.
+ */
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu);
+    }
+}
